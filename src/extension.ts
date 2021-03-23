2146f60abf69124910b4ee257c65797a07d9fc35
Noah Davey <noah.davey@gmail.com>
2021-03-23 22:34:46 +1000

    Fix multiple code lens getting generated for one function keyword

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Disposable } from 'vscode';
 import MyCodeLensProvider from './MyCodeLensProvider';
 
 async function test () {
@@ -8,19 +9,20 @@ async function test () {
 	
 }
 
+interface CodeLensInfo {
+	lineNumber: number,
+	codeLens: Disposable
+
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	let activeEditor = vscode.window.activeTextEditor;
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ComplexityCalculator" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+	let activeEditor = vscode.window.activeTextEditor;
+	let codeLensDisposables: CodeLensInfo [] = []
+
 	let disposable = vscode.commands.registerCommand('ComplexityCalculator.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 
@@ -60,9 +62,15 @@ export function activate(context: vscode.ExtensionContext) {
 			const currentLineText = event.document.lineAt(currentLineNumber).text
 
 			if(currentLineText.includes('function')) {
-				console.log('Winner winner chicken dinner');
-				let codeLensProviderTest = vscode.languages.registerCodeLensProvider(docSelector, new MyCodeLensProvider(currentLineNumber, 0, currentLineNumber, 25))
-				context.subscriptions.push(codeLensProviderTest)			
+				const existingCodeLens = codeLensDisposables.find(x => x.lineNumber === currentLineNumber)
+
+				if(!existingCodeLens) {
+					let codeLensProvider = vscode.languages.registerCodeLensProvider(docSelector, new MyCodeLensProvider(currentLineNumber, 0, currentLineNumber, 25))
+					
+					codeLensDisposables.push({lineNumber: currentLineNumber, codeLens: codeLensProvider})
+					context.subscriptions.push(codeLensProvider)			
+				}
+
 			}				
 		}
 	})
