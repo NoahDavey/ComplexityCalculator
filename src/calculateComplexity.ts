91db9c2d83bd2d55e5809296798fe6a463fb7719
Noah Davey <noah.davey@gmail.com>
2021-04-04 14:06:12 +1000

    Improve complexity regex

--- src/calculateComplexity.ts
+++ src/calculateComplexity.ts
@@ -1,6 +1,6 @@
 function calculateComplexity(func: string): number {
 	let complexity = 1; // Base complexity is 1
-	const regex = new RegExp(/else(?=( {|{))|else if(?=( \(|\())|if(?=(\(| \())/gm);
+	const regex = new RegExp(/else(?=( ?{))|(else if|if)(?=( ?\([A-z !=%0-9()]*\) ?{))/gm);
 	const totalBranchingStatements = (func.match(regex) || []).length;
 
 	complexity += totalBranchingStatements;
